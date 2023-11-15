7c63f2fba0615afdfe7b3ba38396d76f4bf8a202
rushirajd25 <rushirajd25@gmail.com>
2023-11-15 20:21:54 +0530

    feat(blog app): post list view added

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=0).order_by('-created_on')
+    template_name = 'index.html'
